5b8fd9764a6983a2ebf76c2f764674be4ee6e238
NoahDED <noah.dedina@outlook.fr>
2024-01-22 10:46:06 +0100

    Noah-commit2

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var paragraphes = document.getElementsByTagName("p");
 
 for (var i = 0; i < paragraphes.length; i++) {
   paragraphes[i].addEventListener("mouseover", function (event) {
-    var prefixe = "Je suis : ";
+    var prefixe = "Je suis -> ";
     var prenom = this.getAttribute("data-prenom");
     console.log(prefixe + prenom);
   });
