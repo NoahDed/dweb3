d5c53d33ec6772c8a05e68e37256ba3cb2d197ee
NoahDED <noah.dedina@outlook.fr>
2024-01-22 10:28:47 +0100

    Noah viens de faire sa parti

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("hi sugardaddy !")
+
+
+var paragraphes = document.getElementsByTagName("p"); // Get all paragraphs
+
+for (var i = 0; i < paragraphes.length; i++) {
+  paragraphes[i].addEventListener("mouseover", function (event) {
+    var prefixe = "Au survol du paragraphe : ";
+    var prenom = this.getAttribute("data-prenom"); // Get the "data-prenom" attribute
+    console.log(prefixe + prenom);
+  });
+}
